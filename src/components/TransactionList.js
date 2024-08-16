a0cc34e10447ff6cb4ae3403a3709adc2b83b2bd
KevinGitari <kamundikevin@gmail.com>
2024-08-16 08:15:05 +0300

    Update TransactionList.js

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,39 +1,50 @@
-import React, { useState, useEffect } from 'react'; // Added this line
-import SingleTransaction from './SingleTransaction';
+import React, { useEffect, useState } from "react";
+import SingleTransaction from "./SingleTransaction";
 
 function TransactionList() {
   const [transactionList, setTransactionList] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/transactions')
+    fetch("http://localhost:3000/transactions")
       .then((response) => response.json())
       .then((data) => setTransactionList(data))
-      .catch((error) => console.error('Error fetching transactions:', error));
+      .catch((error) => console.error("Error fetching transactions:", error));
   }, []);
 
   return (
+
     <div>
       <h1 className="Transaction-form">Transaction List</h1>
       <table className="table">
+        
         <thead>
+
           <tr>
             <th>Date</th>
             <th>Description</th>
             <th>Category</th>
             <th>Amount</th>
           </tr>
+
         </thead>
+
         <tbody>
+
           {transactionList.map((transaction) => (
             <SingleTransaction 
               key={transaction.id} 
               transaction={transaction} 
             />
           ))}
+
         </tbody>
+
       </table>
+
     </div>
   );
 }
 
 export default TransactionList;
+
+
