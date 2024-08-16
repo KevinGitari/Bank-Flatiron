5e59715c10b3888e33ff92dafb941dcc420002a9
KevinGitari <kamundikevin@gmail.com>
2024-08-16 08:36:43 +0300

    Updated SingleTransaction.js

--- src/components/SingleTransaction.js
+++ src/components/SingleTransaction.js
@@ -1,23 +1,22 @@
 import React from "react";
 
-function SingleTransaction({transaction ,onDelete}) {
-
+function SingleTransaction({ transaction, handleDeleteDelete }) {
   return (
-
     <tr className="tableList">
-
       <td className="tableData">{transaction?.date}</td>
       <td className="tableData">{transaction?.description}</td>
       <td className="tableData">{transaction?.category}</td>
       <td className="tableData">{transaction?.amount}</td>
-      <button className = "del" onClick = {()=>onDelete(transaction)}>Delete Transaction</button>
-
+      <td>
+        <button 
+          className="del" 
+          onClick={() => handleDeleteDelete(transaction.id)}
+        >
+          Delete Transaction
+        </button>
+      </td>
     </tr>
   );
 }
 
 export default SingleTransaction;
-
-
-
-
