69e3c6e5a4d7b5ec71014af8c7f7990d123e9fe8
KevinGitari <kamundikevin@gmail.com>
2024-08-16 08:10:09 +0300

    fixed the deployment errors TransactionList.js

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,57 +1,39 @@
-import React, { useEffect, useState } from "react";
-import SingleTransaction from "./SingleTransaction";
+import React, { useState, useEffect } from 'react'; // Added this line
+import SingleTransaction from './SingleTransaction';
 
 function TransactionList() {
   const [transactionList, setTransactionList] = useState([]);
 
   useEffect(() => {
-    fetch("http://student-back-end-three.vercel.app/transactions")
-       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-      body: JSON.stringify(formData),
-    })
+    fetch('http://localhost:3000/transactions')
       .then((response) => response.json())
       .then((data) => setTransactionList(data))
-      .catch((error) => console.error("Error fetching transactions:", error));
+      .catch((error) => console.error('Error fetching transactions:', error));
   }, []);
 
   return (
-
     <div>
       <h1 className="Transaction-form">Transaction List</h1>
       <table className="table">
-        
         <thead>
-
           <tr>
             <th>Date</th>
             <th>Description</th>
             <th>Category</th>
             <th>Amount</th>
           </tr>
-
         </thead>
-
         <tbody>
-
           {transactionList.map((transaction) => (
             <SingleTransaction 
               key={transaction.id} 
               transaction={transaction} 
             />
           ))}
-
         </tbody>
-
       </table>
-
     </div>
   );
 }
 
 export default TransactionList;
-
-
