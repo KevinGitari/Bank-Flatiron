8e05b9cdef9cabb7b88ddd42ef9750b01c26f0f2
KevinGitari <kamundikevin@gmail.com>
2024-08-16 08:38:51 +0300

    Fixed the deployment errors in TransactionList.js

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,50 +1,66 @@
+import React, { useState, useEffect } from "react";
 import SingleTransaction from "./SingleTransaction";
 
 function TransactionList() {
   const [transactionList, setTransactionList] = useState([]);
 
   useEffect(() => {
+    
     fetch("http://localhost:3000/transactions")
       .then((response) => response.json())
       .then((data) => setTransactionList(data))
       .catch((error) => console.error("Error fetching transactions:", error));
+    
   }, []);
 
+  const handleDelete = (id) => {
+    
+    fetch(`http://localhost:3000/transactions/${id}`, {
+      method: "DELETE",
+      
+    })
+      .then((response) => {
+        if (response.ok) {
+          // Remove the deleted item from the state
+          setTransactionList((prevList) =>
+            prevList.filter((transaction) => transaction.id !== id)
+          );
+        } else {
+          throw new Error("Network response was not ok");
+        }
+      })
+      .catch((error) => console.error("Error deleting transaction:", error));
+  };
+
   return (
-
+    
     <div>
-      <h1 className="Transaction-form">Transaction List</h1>
+      <h1>Transaction List</h1>
       <table className="table">
-        
+    
         <thead>
-
           <tr>
             <th>Date</th>
             <th>Description</th>
             <th>Category</th>
             <th>Amount</th>
+            <th>Actions</th>
           </tr>
-
         </thead>
-
+    
         <tbody>
-
           {transactionList.map((transaction) => (
-            <SingleTransaction 
-              key={transaction.id} 
-              transaction={transaction} 
-            />
+            <SingleTransaction
+              key={transaction.id}
+              transaction={transaction}
+              handleDeleteDelete={handleDelete}
+            />   
           ))}
-
         </tbody>
-
+                
       </table>
-
     </div>
   );
 }
 
 export default TransactionList;
-
-
-
